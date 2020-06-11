fd9acd49b929e6667d4ae46dc9c95c86cb0dd8ae
root <root@vpn1488.vpnservice.company>
2020-06-10 20:03:01 -0400

    proof of concept

---
+++ nagios_cfg_to_json.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+var l = console.log,
+    async = require('async'),
+        fs = require('fs'),
+        FileHound = require('filehound'),
+        nagioscfg2json = require('nagioscfg2json');
+
+var CFG = '/etc/nagios/conf.d/vpnServers.cfg',
+    NAGIOS_DIR = '/etc/nagios',
+    CACHE = '/var/spool/nagios/objects.cache';
+
+
+const files = FileHound.create()
+    .paths(NAGIOS_DIR)
+    .ext('cfg')
+    .size('>16')
+    .find();
+
+files.then(function(files) {
+files = ['/etc/nagios/conf.d/vpnServers.cfg'];
+files = ['/etc/nagios/objects/commands.cfg'];
+
+    async.map(files, function(file, _cb) {
+        var R = {
+            file: file,
+            json: null,
+            err: null,
+        };
+        try {
+l(R);
+
+            nagioscfg2json.fromFile(file, function(json) {
+                R.json = json;
+                _cb(null, R);
+            });
+        } catch (e) {
+            R.err = e;
+            _cb(null, R);
+        }
+    }, function(errs, file_contents) {
+        if (errs) throw errs;
+
+        l(file_contents);
+        process.exit();
+
+
+
+    });
+});
